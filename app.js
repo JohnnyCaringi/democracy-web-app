8e1f060434de9e33de03b6861e6428a9793ef5b6
Caringi <jcaringi@una.edu>
2024-08-29 12:38:58 -0500

    Fixed dynamic website

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express()
 
 console.log("I'm on a node server");
 
+app.use(express.static('./'))
 app.get('/', function (req, res) {
   //outdated way
   //res.send('Hello Node From Express on local devbox :))))')
   res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/styles/style.css')
+  res.sendFile(__dirname + '/scripts/script.js')
 })
 
 app.listen(5000)

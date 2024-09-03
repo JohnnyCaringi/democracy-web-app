7d0b28660bde05303e669c8f81c959bea189b2d4
Caringi <jcaringi@una.edu>
2024-09-03 13:45:59 -0500

    starting with .env

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ console.log("I'm on a node server")
 
 app.use(express.static('./public/'))
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.uri;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', function (req, res) {
   //outdated way
   //res.send('Hello Node From Express on local devbox :))))')
